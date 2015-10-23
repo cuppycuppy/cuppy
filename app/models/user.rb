616524b31b3da40e8f0d680108f5e3bc462f94f8
堀江 麗 <bellerei@horie-no-MacBook-Air.local>
2015-10-23 22:01:25 +0900

    editing user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true,  length: { maximum: 12, message: '12文字以下としてください' }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  has_secure_password
   validates :password, length: { minimum: 6, message: '6文字以上としてください'  }
 end
